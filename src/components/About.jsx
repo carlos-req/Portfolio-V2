0010ac1cdfa12ddd1407c8768eb2c089cb7f7acb
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2022-12-18 00:12:35 -0500

    fix alignemnt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,11 @@ const About = () => {
     <div name='about' className='w-full h-screen bg-[#111111] '>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-            <div className='sm:text-right pb-8 pl-4'>
+            <div className='text-left sm:text-right pb-8 pl-4'>
               <p className='text-zinc-200 text-4xl font-bold inline border-b-4 border-red-500'>
                 About
               </p>
             </div>
-            <div></div>
           </div>
             <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 text-zinc-300'>
               <div className='sm:text-right text-4xl font-bold'>
