70fcdb4a41bafadb01dfeeb02b18eabd4b828553
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2022-12-18 00:22:36 -0500

    fix smooth scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#111111] text-zinc-100'>
       <div className='logo hover:text-red-500'>
-      <Link to="home" offset={50} duration={500}>
+      <Link to="home" smooth={true} duration={500}>
         {logoText}
         {/*<img src={Logo} alt="Logo Image" style={{width:'50px'}} />*/}
         </Link>
@@ -22,27 +22,27 @@ const Navbar = () => {
     {/* menu */}
       <ul className='hidden md:flex'>
       <li>
-          <Link to="home" offset={50} duration={500}>
+          <Link to="home" smooth={true} duration={500}>
             Home
             </Link>
         </li>
         <li>
-          <Link to="about" offset={50} duration={500}>
+          <Link to="about" smooth={true} duration={500}>
             About
             </Link>
         </li>
         <li>
-            <Link to="skills" offset={50} duration={500}>
+            <Link to="skills" smooth={true} duration={500}>
             Skills
             </Link>
         </li>
         <li>
-            <Link to="work" offset={50} duration={500}>
+            <Link to="work" smooth={true} duration={500}>
             Work
             </Link>
         </li>
         <li>
-            <Link to="contact" offset={50} duration={500}>
+            <Link to="contact" smooth={true} duration={500}>
             Contact
             </Link>
         </li>
@@ -57,27 +57,27 @@ const Navbar = () => {
       {/* mobile menu */}
       <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#171717] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'> 
-          <Link onClick={handleClick} to="home" offset={50} duration={500}>
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
             </Link>
         </li>
         <li className='py-6 text-4xl'> 
-          <Link onClick={handleClick} to="about" offset={50} duration={500}>
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
             </Link>
         </li>
         <li className='py-6 text-4xl'> 
-          <Link onClick={handleClick} to="skills" offset={50} duration={500}>
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
             </Link>
         </li>
         <li className='py-6 text-4xl'> 
-          <Link onClick={handleClick} to="work" offset={50} duration={500}>
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Work
             </Link>
         </li>
         <li className='py-6 text-4xl'> 
-          <Link onClick={handleClick} to="contact" offset={50} duration={500}>
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
             </Link>
         </li>
