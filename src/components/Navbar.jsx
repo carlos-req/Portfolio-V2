d6f33e949d82323cd229c99e6b2562220c48dbf8
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2022-12-18 00:27:02 -0500

    Links added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,19 +87,19 @@ const Navbar = () => {
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 '>
-            <a className = 'flex justify-between items-center w-full text-gray-300' href="/">Linkedin <FaLinkedin size={30}/>
+            <a className = 'flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/carlosjrequena/">Linkedin <FaLinkedin size={30}/>
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] '>
-            <a className = 'flex justify-between items-center w-full text-gray-300' href="/">Github <FaGithub size={30}/>
+            <a className = 'flex justify-between items-center w-full text-gray-300' href="https://github.com/carlos-req">Github <FaGithub size={30}/>
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] '>
-            <a className = 'flex justify-between items-center w-full text-gray-300' href="/">Email <HiOutlineMail size={30}/>
+            <a className = 'flex justify-between items-center w-full text-gray-300' href="mailto:cjrequena11@gmail.com">Email <HiOutlineMail size={30}/>
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] '>
-            <a className = 'flex justify-between items-center w-full text-gray-300' href="/">Resume <BsFillPersonLinesFill size={30}/>
+            <a className = 'flex justify-between items-center w-full text-gray-300' href="https://drive.google.com/file/d/1uhGiRFD231N5e68bFBqY6RHq7Eu7cmBM/view?usp=share_link">Resume <BsFillPersonLinesFill size={30}/>
             </a>
           </li>
         </ul>
