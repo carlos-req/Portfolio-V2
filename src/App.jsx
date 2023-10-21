e73cf8ede0a5f670566be05f9bc72584c8e057b6
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-10-21 19:19:20 -0400

    Added Multipage navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import Navbar from "./components/Navbar";
+import Layout from "./Layout";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home/>
-      <About />
-      <Skills />
-      <Work/>
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="skills" element={<Skills />} />
+          <Route path="work" element={<Work />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
